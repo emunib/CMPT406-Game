8ff285edce00ae602e011bae4c4151680d2305c5
Parker Neufeld <parker.neufeld@gmail.com>
2018-01-29 15:50:36 -0600

    Added rewired and got it working with gravity as 2 example actions. Added a line renderer that shows where gravity is pointing.

--- JelloFellow/Assets/Scripts/InputTest.cs
+++ JelloFellow/Assets/Scripts/InputTest.cs
@@ -1,11 +1,25 @@
 ﻿using UnityEngine;
-
+using Rewired;
 public class InputTest : MonoBehaviour {
-	private Rigidbody2D _rigidbody2D;
 
+	//REWIRED
+	private int playerID;
+	private Rewired.Player player;
+
+	private Rigidbody2D _rigidbody2D;
+	private LineRenderer lr;
+	[Range(0.5f,10f)]
+	public float gravityChangePower;
 	private void Start() {
+		lr = GetComponent<LineRenderer> ();
 		_rigidbody2D = GetComponent<Rigidbody2D>();
+		player = ReInput.players.GetPlayer(playerID);
 	}
+
+
+		
+
+
 
 	private void Update () {
 		// Output of the right analog sticks
@@ -15,8 +29,8 @@ public class InputTest : MonoBehaviour {
 		float gravity = 9.81f;
 		
 		// analog inputs
-		float hor = Input.GetAxis("Horizontal_g");
-		float ver = Input.GetAxis("Vertical_g");
+		float hor = player.GetAxis("GravitySetX");
+		float ver = player.GetAxis ("GravitySetY");
 		
 		
 		// if they arent 0
@@ -26,14 +40,24 @@ public class InputTest : MonoBehaviour {
 			// create gravity vector and normalize
 			Vector2 gDir = new Vector2(hor, ver).normalized;
 			Debug.Log("Gravity: " + gDir);
-			
+			float angle;
+			if (hor != 0.0f || ver != 0.0f) {
+				angle = Mathf.Atan2(ver, hor) * Mathf.Rad2Deg;
+				Debug.Log("Angle of stick: " +  angle);
+
+				// Do something with the angle here.
+			}
 			// before we change gravity stop the rigidbody
-			_rigidbody2D.velocity = Vector3.zero;
-			Physics2D.gravity = gDir * gravity;
+			//_rigidbody2D.velocity = Vector3.zero;
+			Physics2D.gravity = gDir * gravity * gravityChangePower;
 			
-			// draw a ray in the direction of the gravity
-			Debug.DrawRay(transform.position, gDir, Color.red);
+
+
+
 		}
+		// draw a ray in the direction of the gravity
+		lr.SetPositions (new Vector3[] { transform.position, transform.position + (Vector3)Physics2D.gravity  });
+
 
 		
 	}
