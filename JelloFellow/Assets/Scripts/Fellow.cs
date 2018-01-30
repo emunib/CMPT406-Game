b7a1c720fb9d26acee1db6bf54ff7f8169f7d8c6
Parker Neufeld <parker.neufeld@gmail.com>
2018-01-29 19:03:20 -0600

    Added movement which works as expected when gravity is up or down, but doesn't behave properly otherwise. Fixed a bug where the floor checker was inconsistent. Jumping seems to work fine now.

--- JelloFellow/Assets/Scripts/Fellow.cs
+++ JelloFellow/Assets/Scripts/Fellow.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using Rewired;
-public class InputTest : MonoBehaviour {
+public class Fellow : MonoBehaviour {
+	//Camera
+	private Camera cam;
 
 	//REWIRED
 	private int playerID;
@@ -8,19 +10,30 @@ public class InputTest : MonoBehaviour {
 
 	private Rigidbody2D _rigidbody2D;
 	private LineRenderer lr;
+
 	[Header("Cooldowns")]
-	//Cooldowns
 	public float jumpCd;
 	public float curJumpCd;
+
 	[Header("Gravity Settings")]
 	[Range(0.5f,10f)]
 	public float gravityChangePower;
+
 	[Header("Movement Settings")]
 	[Range(1f,1000f)]
+	public float moveSpeed;
+	[Range(1f,1000f)]
+	public float controlCutoff; //Movement influence stops if moving faster than this speed
+	[Range(1f,1000f)]
 	public float jumpForce;
+	public bool grounded;
+	public float groundedDistance;
+	private Vector2 groundCheckVector = new Vector2();
+	public LayerMask theFloor;
 
 	public bool auto0Vel = false;
 	private void Start() {
+		cam = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
 		lr = GetComponent<LineRenderer> ();
 		_rigidbody2D = GetComponent<Rigidbody2D>();
 		player = ReInput.players.GetPlayer(playerID);
@@ -39,17 +52,36 @@ public class InputTest : MonoBehaviour {
 		curJumpCd -= Time.deltaTime;
 
 	}
+	//Returns the current angle that the force vector forms
+	private float gravityAngle(){
+		return Mathf.Atan2 (Physics2D.gravity.y, Physics2D.gravity.x);
+	}
+	//Simply returns whether the character can jump
+	public bool IsGrounded() {
+		
+		float angle = gravityAngle ();
 
+		groundCheckVector.x = transform.position.x + Mathf.Cos(angle);
+		groundCheckVector.y = transform.position.y + Mathf.Sin(angle);
+		Debug.DrawLine(transform.position, groundCheckVector, Color.red);
+		Debug.DrawRay(transform.position,new Vector2(Mathf.Cos(angle),Mathf.Sin(angle)),Color.green);
+		RaycastHit2D hit = Physics2D.Raycast(transform.position,new Vector2(Mathf.Cos(angle),Mathf.Sin(angle)), groundedDistance, theFloor);
+		return hit.collider != null;
+	}
 	private void Update () {
-		// Output of the right analog sticks
-		//Debug.Log("H: " + Input.GetAxis("Horizontal_g") + ", V: " + Input.GetAxis("Vertical_g"));
+		cam.transform.position = transform.position - new Vector3(0,0,1);
 		CooldownTick();
+		grounded = IsGrounded ();
 		// constant gravity
 		float gravity = 9.81f;
-		
+
 		// analog inputs
 		float hor = player.GetAxis("GravitySetX");
 		float ver = player.GetAxis ("GravitySetY");
+		float hor_m = player.GetAxis ("Move_H");
+		float ver_m = player.GetAxis ("Move_V");
+
+
 		bool jump = player.GetButton ("Jump");
 		
 		// if they arent 0
@@ -64,29 +96,36 @@ public class InputTest : MonoBehaviour {
 				angle = Mathf.Atan2(ver, hor) * Mathf.Rad2Deg;
 				Debug.Log("Angle of stick: " +  angle);
 
-				// Do something with the angle here.
 			}
-			// before we change gravity stop the rigidbody
-
+			// If the setting is enabled - before we change gravity stop the rigidbody.W
 			if (auto0Vel) {
 				_rigidbody2D.velocity = Vector3.zero;
 			}
+
 			Physics2D.gravity = gDir * gravity * gravityChangePower;
-			
-
-
 
 		}
 
-		if (curJumpCd < 0 && jump) {
+
+		//Basic Movement Controls
+
+		if (curJumpCd < 0 && jump && grounded) {
 			_rigidbody2D.AddForce (-Physics2D.gravity * jumpForce);
 			Debug.Log ("Jumping!");
 			curJumpCd = jumpCd;
+		}
+
+		if (Mathf.Abs (hor_m) > 0 || Mathf.Abs(ver_m)>0 && grounded) {
+			float moveToGAngle = Vector2.Angle (new Vector2 (hor_m, ver_m), Physics2D.gravity);
+			Debug.Log("Movement angle relative to G : " + moveToGAngle);
+
+			if (_rigidbody2D.velocity.magnitude <= controlCutoff) {
+				_rigidbody2D.AddForce (new Vector2 (hor_m, 0) * moveSpeed);
+			}
 		}
 		// draw a ray in the direction of the gravity
 		lr.SetPositions (new Vector3[] { transform.position, transform.position + (Vector3)Physics2D.gravity  });
 
 
-		
 	}
 }
