75a74617112d7926921d03e937bd382df5e135a0
nickdaddy <nicholasb373@gmail.com>
2018-02-04 15:12:19 -0600

    Added simple tracksaw prefab. Interaction with player not finished as I'm not sure how dmg is dealt at this point

---
+++ JelloFellow/Assets/Scripts/trackSawmovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackSawmovement : MonoBehaviour {
+
+	private Vector2 posA,posB, nexPos;
+	public float speed;
+
+	public Transform childtrans;
+
+	public Transform transformB;
+
+	private void move(){
+		childtrans.localPosition = Vector2.MoveTowards (childtrans.localPosition, nexPos, speed * Time.deltaTime);
+
+		if (Vector2.Distance (childtrans.localPosition, nexPos) <= 0.1) {
+			nexPos = nexPos!= posA ? posA: posB;
+
+		}
+
+	}
+
+	// Use this for initializatio  n
+	void Start () {
+		posA = childtrans.localPosition;
+		posB = transformB.localPosition;
+		nexPos = posB;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move ();
+	}
+}
