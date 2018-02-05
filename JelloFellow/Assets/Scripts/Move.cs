c429acecedab5e63f1ad1e066c8fdbeb7134b908
Ehtasham <ehm011@mail.usask.ca>
2018-02-05 12:30:19 -0600

    add basic movement
    appears to be good overall, buggy in tight spaces

---
+++ JelloFellow/Assets/Scripts/Move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+	public float Power;
+	public float Speed;
+	private Rigidbody2D[] _children;
+	// Use this for initialization
+	private void Start ()
+	{
+		_children = GetComponentsInChildren<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			foreach (var child in _children)
+			{
+				child.AddForce(Vector2.up * Power, ForceMode2D.Impulse);
+			}
+		}
+
+		foreach (var child in _children)
+		{
+			child.AddForce(Input.GetAxis("Horizontal") * Vector2.right * Speed);
+		}
+	}
+}
