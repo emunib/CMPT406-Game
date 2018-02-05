96b5227cf2008f8045bfde462bcdef2a358a4522
Ehtasham <ehm011@mail.usask.ca>
2018-02-05 11:45:19 -0600

    add basic camera follow script

---
+++ JelloFellow/Assets/Scripts/Follow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+	public Transform Target;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Camera.main.transform.position = new Vector3(Target.position.x, Target.position.y, -10);
+	}
+}
