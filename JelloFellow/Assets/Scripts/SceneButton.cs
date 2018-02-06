8aadc4f66d067c6b4200d461cad51256dece3932
Acpeters <austin.clarke.peters@gmail.com>
2018-02-05 21:34:42 -0600

    Added script for selecting scenes when button is pressed
    also added a second test sample scene to check if buttons work

---
+++ JelloFellow/Assets/Scripts/SceneButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButton : MonoBehaviour
+{
+
+    public Object theSceneLinkedByThisButton;
+    
+    public void LoadByName()
+    {
+        SceneManager.LoadScene(theSceneLinkedByThisButton.name);
+    }
+}
