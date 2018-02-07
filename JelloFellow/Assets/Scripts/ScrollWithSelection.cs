c691f9ebf3376caae71ccce58fe4a9b2937dd18b
Acpeters <austin.clarke.peters@gmnail.com>
2018-02-06 21:18:26 -0600

    Fixed some formatting issues

--- JelloFellow/Assets/Scripts/ScrollWithSelection.cs
+++ JelloFellow/Assets/Scripts/ScrollWithSelection.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/*
+
+    Based code on snipet from https://gamedev.stackexchange.com/questions/112662/unity-autoscroll-when-selecting-buttons-out-of-viewport
+
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,80 +12,91 @@ using UnityEngine.UI;
 public class ScrollWithSelection : MonoBehaviour 
 {
 	[SerializeField]
-	private float               m_lerpTime;
-	private ScrollRect          m_scrollRect;
-	private Button[]            m_buttons;
-	public int                 m_index;
-	public float               m_verticalPosition;
-	private bool                m_up;
-	private bool                m_down;
-	private bool                m_left;
-	private bool                m_right;
-	public int 				level;
-	public int 				ammountOfLevels;
+	private float		lerpTime;
+	private ScrollRect 	scrollRect;
+	private Button[]	buttonsArray;
+	public int			index;
+	public float		yPos;
+	private bool		upInput;
+	private bool		downInput;
+	private bool		leftInput;
+	private bool		rightInput;
+	public int			level;
+	public int			ammountOfLevels;
 
 	public void Start()
 	{
-		m_scrollRect        = GetComponent<ScrollRect>();
-		m_buttons           = GetComponentsInChildren<Button>();
-		m_buttons[m_index].Select();
+		scrollRect = GetComponent<ScrollRect>();
+		buttonsArray = GetComponentsInChildren<Button>();
+		buttonsArray[index].Select();
 		level = 0;
-		if(m_buttons.Length%4 == 0){
-			ammountOfLevels = m_buttons.Length / 4;
+		if(buttonsArray.Length%4 == 0)
+		{
+			ammountOfLevels = buttonsArray.Length / 4;
 		}
-		else {
-			ammountOfLevels = (m_buttons.Length / 4) + 1;
+		else 
+		{
+			ammountOfLevels = (buttonsArray.Length / 4) + 1;
 		}
-		m_verticalPosition  = 1f - ((float)m_index / (m_buttons.Length - 1));
+		yPos  = 1f - ((float)index / (buttonsArray.Length - 1));
 		
 	}
 
 	public void Update()
 	{
-		m_up    = Input.GetKeyDown(KeyCode.UpArrow);
-		m_down  = Input.GetKeyDown(KeyCode.DownArrow);
-		m_left    = Input.GetKeyDown(KeyCode.LeftArrow);
-		m_right  = Input.GetKeyDown(KeyCode.RightArrow);
+		upInput    = Input.GetKeyDown(KeyCode.UpArrow);
+		downInput  = Input.GetKeyDown(KeyCode.DownArrow);
+		leftInput    = Input.GetKeyDown(KeyCode.LeftArrow);
+		rightInput  = Input.GetKeyDown(KeyCode.RightArrow);
 
-		if (m_up ^ m_down ^ m_left ^ m_right)
+		if (upInput ^ downInput ^ leftInput ^ rightInput)
 		{
-			if (m_up)
+			if (upInput)
 			{
-				if(m_index>=4)
-					m_index = Mathf.Clamp(m_index - 4, 0, m_buttons.Length - 1);
-				if(level>0)
+				if (index >= 4)
+				{
+					index = Mathf.Clamp(index - 4, 0, buttonsArray.Length - 1);
+				}
+
+				if (level > 0)
+				{
 					level--;
-//				m_buttons[m_index].Select();
-//				m_verticalPosition = 1f - ((float)m_index / (m_buttons.Length - 1));
+				}
 			}
-			else if (m_down)
+			else if (downInput)
 			{
-				if(m_index<= m_buttons.Length - 5)
-					m_index = Mathf.Clamp(m_index + 4, 0, m_buttons.Length - 1);
-				if(level < ammountOfLevels - 1)
+				if (index <= buttonsArray.Length - 5)
+				{
+					index = Mathf.Clamp(index + 4, 0, buttonsArray.Length - 1);
+				}
+
+				if (level < ammountOfLevels - 1)
+				{
 					level++;
-//				m_buttons[m_index].Select();
-//				m_verticalPosition = 1f - ((float)m_index / (m_buttons.Length - 1));
+				}
 			}
-			else if(m_left)
+			else if(leftInput)
 			{
-				if(m_index % 4 != 0)
-					m_index = Mathf.Clamp(m_index - 1, 0, m_buttons.Length - 1);
+				if (index % 4 != 0)
+				{
+					index = Mathf.Clamp(index - 1, 0, buttonsArray.Length - 1);
+				}
 			}
 			else
 			{
-				if(m_index % 4 != 3)
-					m_index = Mathf.Clamp(m_index + 1, 0, m_buttons.Length - 1);
+				if (index % 4 != 3)
+				{
+					index = Mathf.Clamp(index + 1, 0, buttonsArray.Length - 1);
+				}
 			}
 
-			m_buttons[m_index].Select();
-//			m_verticalPosition = 1f - ((float)m_index / (m_buttons.Length - 1));
-//			m_verticalPosition = 1 - (m_index / ((m_buttons.Length-1)/ 4));
-			if(ammountOfLevels > 2)
-            	m_verticalPosition = 1f - ((float)level / (ammountOfLevels - 1));
+			buttonsArray[index].Select();
+			if (ammountOfLevels > 2)
+			{
+				yPos = 1f - ((float) level / (ammountOfLevels - 1));
+			}
 
 		}
-
-		m_scrollRect.verticalNormalizedPosition = Mathf.Lerp(m_scrollRect.verticalNormalizedPosition, m_verticalPosition, Time.deltaTime / m_lerpTime);
+		scrollRect.verticalNormalizedPosition = Mathf.Lerp(scrollRect.verticalNormalizedPosition, yPos, Time.deltaTime / lerpTime);
 	}
 }
