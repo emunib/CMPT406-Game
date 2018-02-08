eb3f76b3aa83e809e7c64a059c00c87542d5239e
Cody <cwb581@mail.usask.ca>
2018-02-08 16:38:57 -0600

    Replacing grav field with a dynamic collider rather than checking it with Overlapping colliders

--- JelloFellow/Assets/Scripts/FellowPlayer.cs
+++ JelloFellow/Assets/Scripts/FellowPlayer.cs
@@ -8,32 +8,30 @@ public class FellowPlayer : MonoBehaviour {
   /// </summary>
   private Input2D input;
 
-  private HashSet<GameObject> objects_in_gravityfield;
-  
   private void Start() {
-    objects_in_gravityfield = new HashSet<GameObject>();
-    
     /* get the input from the controller */
     InputController _inputcontroller = GameObject.FindGameObjectWithTag("InputController").GetComponent<InputController>();
     input = _inputcontroller.GetInput();
   }
 
-  private void Update() {
-    /* next step is to know when the object us outside its radius */
-    
-    /* if the gameobject is within the GravityFieldRadius add it to the HashSet */
-    /* Hashset only stores only unique values so it doesnt matter if it registers again */
-    foreach (Collider2D object_in_radius in Physics2D.OverlapCircleAll(transform.position, GravityFieldRadius)) {
-      if (object_in_radius.gameObject.Equals(gameObject)) {
-        GravityComponent gravity_component = object_in_radius.gameObject.GetComponent<GravityComponent>();
-        if (gravity_component != null) {
-          gravity_component.InGravityField();
-          objects_in_gravityfield.Add(object_in_radius.gameObject);
-          Debug.Log(object_in_radius.gameObject.name);
-        }
-      }
+  private void OnCollisionEnter2D(Collision2D other)
+  {
+    GravityComponent grav_component = other.gameObject.GetComponent<GravityComponent>();
+    if (grav_component != null)
+    {
+      grav_component.InGravityField();
     }
   }
+
+  private void OnCollisionExit2D(Collision2D other)
+  {
+    GravityComponent grav_component = other.gameObject.GetComponent<GravityComponent>();
+    if (grav_component != null)
+    {
+      grav_component.OutsideGravityField();
+    }
+  }
+
 
   private void OnDrawGizmos() {
     Gizmos.color = new Color(1f, 0f, 0f, 0.2f);
