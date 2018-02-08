f7836c405846877ceb1d704a0969e62ba0ff9b58
Nicholas Brunoro <nicholasb373@gmail.com>
2018-02-08 12:01:06 -0600

    Gravity Fields minimal implementation. Figured out how to disable and renable along an axis. Needs to be updated to the proposed changes talked about in tuesdays meeting. Also Need to figure out a sprite or a 2d lighting technique of some sort

--- JelloFellow/Assets/Scripts/disableGravity.cs
+++ JelloFellow/Assets/Scripts/disableGravity.cs
@@ -1,41 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class disableGravity : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.attachedRigidbody) {
-			Vector2 vel = new Vector2 (other.attachedRigidbody.velocity.x, 0f);
-			other.attachedRigidbody.gravityScale= 0f;
+		
  			Debug.Log ("object entered field");
+			other.attachedRigidbody.gravityScale = 0f;
+
+			//Vector2 vel = other.attachedRigidbody.velocity;
+			//other.attachedRigidbody.velocity = new Vector2(vel.x,0);
+			
+
 		}
 	}
 
 	void OnTriggerStay2D(Collider2D other){
 		if (other.attachedRigidbody) {
-			Vector2 vel = new Vector2 (0.0f, 0f);
-			other.attachedRigidbody.gravityScale= 0f;
-			other.attachedRigidbody.velocity = vel;
-			Debug.Log ("Object is at rest");
+			
+			Debug.Log ("trying to push");
+			Vector2 g = new Vector2(Physics2D.gravity.x,0);
+			
+			other.attachedRigidbody.AddForce(g);
 		}
 	}
-		/*
+		
 	void OnTriggerExit2D(Collider2D other){
 		if (other.attachedRigidbody) {
 			other.attachedRigidbody.gravityScale = 1f;
 			Debug.Log ("leaving grav field");
 		}
 	}
-	*/
+	
+	
+	
+	
 }
+
+
