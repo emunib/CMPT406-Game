62b665f626cd480f0705c2b64edc524d2030969a
Ehtasham <ehm011@mail.usask.ca>
2018-02-11 23:32:04 -0600

    level setup and minor changes

--- JelloFellow/Assets/Scripts/ScaleSoftbody.cs
+++ JelloFellow/Assets/Scripts/ScaleSoftbody.cs
@@ -2,57 +2,66 @@
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class ScaleSoftbody : MonoBehaviour
 {
-    public bool Resize;
-    public bool RevertToDefaults;
     [Range(0.3f, 2f)] public float Scale = 1;
+    public bool RevertToDefaults;
+    public bool UpdateSize;
 
     private const float DefaultInnerNodeSize = 1f;
     private const float DefaultOuterNodeSize = 1.5f;
     private const float DefaultSpringDistance = 1f;
-    
-    void Update()
+
+    private void Start()
+    {
+        Resize();
+    }
+
+    private void Update()
     {
         if (RevertToDefaults) Scale = 1;
         
-        if (Resize || RevertToDefaults)
-        {
-            Resize = false;
-            RevertToDefaults = false;
-            
-            List<Transform> transforms = new List<Transform>();
-            
-            transforms.Add(GameObject.Find("Softbody/Centre").transform);
-            for (var i = 1; i <= 6; i++)
-            {
-                transforms.Add(GameObject.Find("Softbody/C" + i).transform);
-            }
-
-            foreach (var tf in transforms)
-            {
-                tf.localScale = new Vector3(DefaultInnerNodeSize * Scale, DefaultInnerNodeSize * Scale, 1);
-            }
-            
-            transforms.Clear();
-            
-            for (var i = 1; i <= 9; i++)
-            {
-                transforms.Add(GameObject.Find("Softbody/O" + i).transform);
-            }
-
-            foreach (var tf in transforms)
-            {
-                tf.localScale = new Vector3(DefaultOuterNodeSize * Scale, DefaultOuterNodeSize * Scale, 1);
-            }
-            
-            var springs = GetComponentsInChildren<SpringJoint2D>();
-
-            foreach (var sp in springs)
-            {
-                sp.distance = DefaultSpringDistance * Scale;
-            }
-        }
+        if (UpdateSize || RevertToDefaults) Resize();
+        
     }
+
+    private void Resize()
+    {
+        UpdateSize = false;
+        RevertToDefaults = false;
+            
+        List<Transform> transforms = new List<Transform>();
+            
+        transforms.Add(GameObject.Find("Softbody/Centre").transform);
+        for (var i = 1; i <= 6; i++)
+        {
+            transforms.Add(GameObject.Find("Softbody/C" + i).transform);
+        }
+
+        foreach (var tf in transforms)
+        {
+            tf.localScale = new Vector3(DefaultInnerNodeSize * Scale, DefaultInnerNodeSize * Scale, 1);
+        }
+            
+        transforms.Clear();
+            
+        for (var i = 1; i <= 9; i++)
+        {
+            transforms.Add(GameObject.Find("Softbody/O" + i).transform);
+        }
+
+        foreach (var tf in transforms)
+        {
+            tf.localScale = new Vector3(DefaultOuterNodeSize * Scale, DefaultOuterNodeSize * Scale, 1);
+        }
+            
+        var springs = GetComponentsInChildren<SpringJoint2D>();
+
+        foreach (var sp in springs)
+        {
+            sp.distance = DefaultSpringDistance * Scale;
+        }
+    } 
 }
