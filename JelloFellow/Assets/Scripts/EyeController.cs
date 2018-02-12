552ddcf936ee1696f3222833f9c603cf315a1e6e
Ehtasham <ehm011@mail.usask.ca>
2018-02-12 17:45:44 -0600

    fix eyes positioning/rotation

--- JelloFellow/Assets/Scripts/EyeController.cs
+++ JelloFellow/Assets/Scripts/EyeController.cs
@@ -1,37 +1,38 @@
 ﻿using UnityEngine;
 
-public class EyeController : MonoBehaviour {
-	private Input2D _input;
-	public Transform Parent;
-	private Vector2 _original;
-	private Vector2 _vec;
-	private Vector2 _velocity;
+public class EyeController : MonoBehaviour
+{
+    private Input2D _input;
+    public Transform Parent;
+    private Vector2 _original;
+    private Vector2 _vec;
+    private Vector2 _velocity;
 
-	private float _xScale;
-	private float _yScale;
+    private float _xScale;
+    private float _yScale;
 
-	private void Awake()
-	{
-		_xScale = Parent.transform.localScale.x * transform.localScale.x;
-		_yScale = Parent.transform.localScale.y * transform.localScale.y;
-	} 
+    private void Awake()
+    {
+        _xScale = Parent.transform.localScale.x * transform.localScale.x;
+        _yScale = Parent.transform.localScale.y * transform.localScale.y;
+    }
 
-	private void Start()
-	{
-		_input = GameObject.FindGameObjectWithTag("InputController").GetComponent<InputController>().GetInput();
-		_original = transform.localPosition;
-	}
+    private void Start()
+    {
+        _input = GameObject.FindGameObjectWithTag("InputController").GetComponent<InputController>().GetInput();
+        _original = transform.localPosition;
+    }
 
-	private void Update ()
-	{
-		var dir = new Vector2(_input.GetHorizontalMovement(), _input.GetVerticalMovement());
-		dir = dir.normalized ;
-		dir = new Vector2(dir.x * _xScale, dir.y * _yScale);
-		
-		_vec = Vector2.SmoothDamp(_vec,
-			new Vector2(dir.x, dir.y), ref _velocity, 0.2f,
-			Mathf.Infinity, Time.deltaTime);
+    private void Update()
+    {
+        var dir = new Vector2(_input.GetHorizontalMovement(), _input.GetVerticalMovement());
+        dir = dir.normalized;
+        dir = new Vector2(dir.x * _xScale, dir.y * _yScale);
 
-		transform.localPosition = new Vector2(_vec.x + _original.x, _vec.y + _original.y);
-	}
+        dir = transform.InverseTransformDirection(dir);
+
+        _vec = Vector2.SmoothDamp(_vec, new Vector2(dir.x, dir.y), ref _velocity, 0.2f, Mathf.Infinity, Time.deltaTime);
+
+        transform.localPosition = new Vector2(_vec.x + _original.x , _vec.y + _original.y);
+    }
 }
--- JelloFellow/Assets/Scripts/FaceController.cs
+++ JelloFellow/Assets/Scripts/FaceController.cs
@@ -7,6 +7,8 @@ public class FaceController : MonoBehaviour
 	private List<Transform> _nodes = new List<Transform>();
 	private Vector2 _vec;
 	private Vector2 _velocity;
+	private Vector2 _velocity2;
+	private Vector2 _vec2;
 	
 	// Use this for initialization
 	private void Start () {
@@ -35,10 +37,18 @@ public class FaceController : MonoBehaviour
 			minY = Mathf.Min(minY, node.position.y);
 			maxY = Mathf.Max(maxY, node.position.y);
 		}
+
+		var gravityOpposite = -Physics2D.gravity.normalized * 0.7f;
+		
+		_vec2 = Vector2.SmoothDamp(_vec2, gravityOpposite, ref _velocity2, 0.1f,
+			Mathf.Infinity, Time.deltaTime); // gradually move towards target;
+
+		transform.up = _vec2;
 		
 		_vec = Vector2.SmoothDamp(_vec, new Vector2(_input.GetHorizontalMovement() * transform.localScale.x, _input.GetVerticalMovement()/2f * transform.localScale.y), ref _velocity, 0.2f,
 			Mathf.Infinity, Time.deltaTime); // gradually move towards target
 		
-		transform.position = new Vector2((maxX - minX) * 0.5f + minX + _vec.x, (maxY - minY) * 0.8f + minY + _vec.y);
+		transform.position = new Vector2((maxX - minX) * 0.5f + minX + _vec.x + _vec2.x
+			, (maxY - minY) * 0.5f + minY + _vec.y + _vec2.y);
 	} 
 }
