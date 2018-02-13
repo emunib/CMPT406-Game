8ac49903ce2cf4768699d7789bb84767088cd19a
Acpeters <austin.clarke.peters@gmail.com>
2018-02-12 23:28:09 -0600

    Made control of your selection more natural feeling

--- JelloFellow/Assets/Scripts/SceneSelectorScene.cs
+++ JelloFellow/Assets/Scripts/SceneSelectorScene.cs
@@ -22,33 +22,37 @@ public class SceneSelectorScene : MonoBehaviour
 	private bool		leftInput;
 	private bool		rightInput;
 	private bool 		select;
-	private Input2D 	_input;
-	private int			level;
-	private int			ammountOfLevels;
+	private Input2D 	input;
+	private int			row;
+	private int			ammountOfRows;
+
 
 	public void Start()
 	{
-		InvokeRepeating("CheckForControllerInput", 0.0f, 0.07f);
-		_input = GameObject.FindGameObjectWithTag("InputController").GetComponent<InputController>().GetInput();
+		InvokeRepeating("CheckForControllerInput", 0.0f, 0.1f);
+		input = GameObject.FindGameObjectWithTag("InputController").GetComponent<InputController>().GetInput();
 		scrollRect = GetComponent<ScrollRect>();
 		buttonsArray = GetComponentsInChildren<Button>();
 		buttonsArray[index].Select();
-		level = 0;
+		row = 0;
+		lerpTime = 0.01f;
+        //Checks to see how many rows of scenes there are
 		if(buttonsArray.Length%4 == 0)
 		{
-			ammountOfLevels = buttonsArray.Length / 4;
+			ammountOfRows = buttonsArray.Length / 4;
 		}
 		else 
 		{
-			ammountOfLevels = (buttonsArray.Length / 4) + 1;
+			ammountOfRows = (buttonsArray.Length / 4) + 1;
 		}
 		yPos  = 1f - ((float)index / (buttonsArray.Length - 1));
 		
 	}
 
 	public void Update(){
-		select = _input.GetJumpButtonDown();
-
+		
+		//If selected click the button
+		select = input.GetJumpButtonDown();
 		if (select)
 		{
 			buttonsArray[index].onClick.Invoke();
@@ -57,45 +61,46 @@ public class SceneSelectorScene : MonoBehaviour
 
 	public void CheckForControllerInput()
 	{
-		upInput = _input.GetVerticalMovement() > 0;
-		downInput = _input.GetVerticalMovement() < 0;
-		leftInput = _input.GetHorizontalMovement() < 0;
-		rightInput = _input.GetHorizontalMovement() > 0;
+		float hor_m = input.GetHorizontalMovement();
+		float ver_m = input.GetVerticalMovement();
 
-		if (upInput ^ downInput ^ leftInput ^ rightInput)
-		{
-			if (upInput)
+		if (Mathf.Abs (hor_m) > 0 || Mathf.Abs (ver_m) > 0) {
+			//Move Up
+			if (ver_m > 0)
 			{
 				if (index >= 4)
 				{
 					index = Mathf.Clamp(index - 4, 0, buttonsArray.Length - 1);
 				}
 
-				if (level > 0)
+				if (row > 0)
 				{
-					level--;
+					row--;
 				}
 			}
-			else if (downInput)
+			//Move Down
+			else if (ver_m < 0)
 			{
 				if (index <= buttonsArray.Length - 5)
 				{
 					index = Mathf.Clamp(index + 4, 0, buttonsArray.Length - 1);
 				}
 
-				if (level < ammountOfLevels - 1)
+				if (row < ammountOfRows - 1)
 				{
-					level++;
+					row++;
 				}
 			}
-			else if (leftInput)
+			//Move Left
+			if (hor_m < 0)
 			{
 				if (index % 4 != 0)
 				{
 					index = Mathf.Clamp(index - 1, 0, buttonsArray.Length - 1);
 				}
 			}
-			else if(rightInput)
+			// Move Right
+			else if(hor_m > 0)
 			{
 				if (index % 4 != 3)
 				{
@@ -104,12 +109,13 @@ public class SceneSelectorScene : MonoBehaviour
 			}
 
 			buttonsArray[index].Select();
-			if (ammountOfLevels > 2)
+            //Don't scroll the screen if there are 2 or less rows of scenes
+			if (ammountOfRows > 2)
 			{
-				yPos = 1f - ((float) level / (ammountOfLevels - 1));
+				yPos = 1f - ((float) row / (ammountOfRows - 1));
 			}
-
 		}
-		scrollRect.verticalNormalizedPosition = Mathf.Lerp(scrollRect.verticalNormalizedPosition, yPos, Time.deltaTime / lerpTime);	
+		//Scroll the screen to the proper hight
+		scrollRect.verticalNormalizedPosition = Mathf.Lerp(scrollRect.verticalNormalizedPosition, yPos, Time.deltaTime / lerpTime);
 	}
 }
