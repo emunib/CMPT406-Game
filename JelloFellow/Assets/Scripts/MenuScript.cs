bb2be650786c6b60883fa7c936d5c4a4a348e419
Acpeters <austin.clarke.peters@gmail.com>
2018-02-16 14:46:42 -0600

    Mapped Play to explicity go to TestGravity
    Also tested funcitonality of menus

--- JelloFellow/Assets/Scripts/MenuScript.cs
+++ JelloFellow/Assets/Scripts/MenuScript.cs
@@ -13,7 +13,7 @@ public class MenuScript : MonoBehaviour {
 	}
 	// Start game from level 1
 	public void PlayGame () {
-		SceneManager.LoadScene(1);
+		SceneManager.LoadScene("TestingGravity");
 
 	}
 	//Level Selector Button
