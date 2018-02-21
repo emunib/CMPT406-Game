b674e52977dd9950b08fec9f0916385e43e1b32f
Carter Hill <carterghill@gmail.com>
2018-02-21 13:05:13 -0600

    Created collectable items script, which will act as a static class to store all collectables

---
+++ JelloFellow/Assets/CollectedItems.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectedItems : MonoBehaviour {
+
+	public static CollectedItems items;
+
+	// Use this for initialization, happens before start
+	void Awake () {
+		
+		// if items doesn't exist, make this 
+		if (items == null) {
+			DontDestroyOnLoad (gameObject);
+			items = this;
+		} else if (items != this) {
+			// Destroy this object if items already exists
+			Destroy (gameObject);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
