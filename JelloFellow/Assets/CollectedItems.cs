723aeed3d0df01304172851a9d8a785f01b49b15
Carter Hill <carterghill@gmail.com>
2018-02-21 13:46:12 -0600

    Collected Items now has a string list of all collectables gathered. It is output as GUI on the screen (may change) To do: add items to list when collected

--- JelloFellow/Assets/CollectedItems.cs
+++ JelloFellow/Assets/CollectedItems.cs
@@ -4,24 +4,46 @@ using UnityEngine;
 
 public class CollectedItems : MonoBehaviour {
 
-	public static CollectedItems items;
+	public static CollectedItems script;
+
+	private LinkedList<string> items;
 
 	// Use this for initialization, happens before start
 	void Awake () {
 		
 		// if items doesn't exist, make this 
-		if (items == null) {
+		if (script == null) {
+			
 			DontDestroyOnLoad (gameObject);
-			items = this;
-		} else if (items != this) {
+			script = this;
+			items = new LinkedList<string> ();
+			items.AddLast ("Haha nice");
+			items.AddLast ("Rock on");
+
+		} else if (script != this) {
+			
 			// Destroy this object if items already exists
 			Destroy (gameObject);
+
 		}
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void OnGUI () {
 		
+		LinkedListNode<string> current = items.First;
+
+		int i = 0;
+		while (current != null) {
+			GUI.Label (new Rect (10, 10 + (i * 40), 100, 30), current.Value);
+			current = current.Next;
+			i++;
+		}
+	}
+
+	void AddItem(){
+		string thing = "Collectable " + items.Count;
+		items.AddLast (thing);
 	}
 }
