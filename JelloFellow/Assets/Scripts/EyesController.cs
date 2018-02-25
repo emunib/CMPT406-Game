4b4dfc6976a8775a67b28c51d584c0c673bfe363
Ehtasham <ehm011@mail.usask.ca>
2018-02-25 12:47:12 -0600

    add some eye movement

---
+++ JelloFellow/Assets/Scripts/EyesController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class EyesController : MonoBehaviour
+{
+    public Vector2 OffsetFromCentre;
+    [Range(0, 1)]public float SmoothTime;
+    private Input2D _input;
+    private Vector2 _vel;
+    private Vector2 _position;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        _input = GameObject.FindGameObjectWithTag("InputController").GetComponent<InputController>().GetInput();
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        var targetPos = OffsetFromCentre + new Vector2(_input.GetHorizontalLeftStick(), _input.GetVerticalLeftStick());
+        _position = Vector2.SmoothDamp(_position, targetPos, ref _vel, SmoothTime, Mathf.Infinity, Time.deltaTime);
+        transform.localPosition = _position;
+    }
+}
