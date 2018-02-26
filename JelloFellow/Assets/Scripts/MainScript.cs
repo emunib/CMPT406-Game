60911c33a9a5983e60fa993f2a779dcf005f5c59
Ehtasham <ehm011@mail.usask.ca>
2018-02-25 23:03:52 -0600

    modify MainScript

--- JelloFellow/Assets/Scripts/MainScript.cs
+++ JelloFellow/Assets/Scripts/MainScript.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 
-public class MainScript : MonoBehaviour {
-  [CustomLabel("Gravity Force")] [Tooltip("Force at which gravity is applied to objects.")] [SerializeField]
-  private float gravity_force = 9.81f;
+public class MainScript : MonoBehaviour
+{
+    [CustomLabel("Gravity Force")] [Tooltip("Force at which gravity is applied to objects.")] [SerializeField]
+    private float gravity_force = 9.81f;
+    public static MainScript mainScript;
 
-  private void Awake() {
-    if (!GameObject.FindGameObjectWithTag("Main")) {
-      DontDestroyOnLoad(gameObject);
-    } else {
-//      Destroy(gameObject);
+    void Awake()
+    {
+        if (!mainScript)
+        {
+            mainScript = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
     }
-  }
 
-  public float GravityForce() {
-    return gravity_force;
-  }
+    public float GravityForce()
+    {
+        return gravity_force;
+    }
 }
