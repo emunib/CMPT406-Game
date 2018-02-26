7ab825ad18db04a8d54bf4a918a7101f237f0613
Nicholas Brunoro <nicholasb373@gmail.com>
2018-02-26 15:02:42 -0600

    Bouncy goo

---
+++ JelloFellow/Assets/Scripts/GooBounceInner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooBounceInner : MonoBehaviour {
+	public string playerLayer = "SlimeEffector";
+
+	public bool debugReflectRay;
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.layer == LayerMask.NameToLayer(playerLayer)) {
+			Debug.Log("Player came in");
+
+			//Get Normal
+			RaycastHit2D ray = Physics2D.Raycast(other.transform.position, other.attachedRigidbody.velocity);
+			Vector2 dir = Vector3.Reflect(other.attachedRigidbody.velocity, ray.normal);
+
+			FellowPlayer center = other.transform.parent.GetComponentInChildren<FellowPlayer>();
+			center.SendMessage("AddVelocity",dir*2);
+			
+			
+		}
+	}
+
+
+	
+	private void OnTriggerStay2D(Collider2D other) {
+		if (debugReflectRay) {
+			if (other.gameObject.layer == LayerMask.NameToLayer(playerLayer)) {
+				Debug.Log("Player came in");
+
+				//Get Normal
+				RaycastHit2D ray = Physics2D.Raycast(other.transform.position, other.attachedRigidbody.velocity);
+				Vector2 dir = Vector3.Reflect(other.attachedRigidbody.velocity, ray.normal);
+
+				Debug.DrawRay(other.transform.position, dir * 100);
+
+
+			}
+		}
+	}
+}
---
+++ JelloFellow/Assets/Scripts/ZGooScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZGooScript : MonoBehaviour {
+
+	public string playerLayer = "SlimeEffector";
+	public string gooLayer = "Goo";
+
+	private void Start() {
+		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer(playerLayer),LayerMask.NameToLayer(gooLayer));
+	}
+
+	
+	
+	
+}
