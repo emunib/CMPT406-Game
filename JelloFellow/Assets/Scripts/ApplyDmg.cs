9f9a1380bcdd33a623de11bf3dc8ed7c8c7bf455
Nicholas Brunoro <nicholasb373@gmail.com>
2018-02-26 18:16:26 -0600

    Changed the apply dmg script to not be on a mask as it is having issues with git.

--- JelloFellow/Assets/Scripts/ApplyDmg.cs
+++ JelloFellow/Assets/Scripts/ApplyDmg.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ApplyDmg : MonoBehaviour {
-	public LayerMask playerLayer = -1;
-	
+	//public LayerMask playerLayer = -1;
+
+	public string playerLayer = "SlimeEffector";
 
 	private void OnTriggerEnter2D(Collider2D other) {
-
+/*
 		if (playerLayer == (playerLayer| (1<<other.gameObject.layer))) {
 			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}*/
+		if (playerLayer == LayerMask.LayerToName(other.gameObject.layer)) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
 		}
+		
 		//Kill enemy
 		else if (other.gameObject.CompareTag("Enemy")) {
 			Destroy(other.gameObject);
