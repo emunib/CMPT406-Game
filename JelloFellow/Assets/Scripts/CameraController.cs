a965c8d1c3782c21ec319db9cbcf310dc92f021f
Rutvik Sanghavi (rjs580) <rutvik.sanghavi@usask.ca>
2018-02-28 10:21:41 -0600

    Test with processing and DOF

--- JelloFellow/Assets/Scripts/CameraController.cs
+++ JelloFellow/Assets/Scripts/CameraController.cs
@@ -3,7 +3,7 @@
 // simple camera follow script with smooth movement as well as smooth zoom in/out based on target's velocity
 public class CameraController : MonoBehaviour
 {
-    [Header("Gameobjects")] public Transform Target; // the gameobject the camera will follow
+    [Header("Gameobjects")] public Transform Target = null; // the gameobject the camera will follow
 
     public JellySprite Jelly; // if no target is defined then the camera will use the jelly object as a target
 
@@ -26,7 +26,7 @@ public class CameraController : MonoBehaviour
     // update camera after target movement has occurred in Update()
     private void LateUpdate()
     {
-        if (!Target && Jelly)
+        if (Target != null && Jelly)
         {  // if the jelly is defined instead of the target then use the jelly's centre point as a target
             Target = Jelly.ReferencePoints[0].transform;
         }
@@ -38,33 +38,33 @@ public class CameraController : MonoBehaviour
 
     private void Move()
     {
-        Vector2 cur = transform.position;
-        Vector2 tar = Target.position;
+        if (Target != null) {
+            Vector2 cur = transform.position;
+            Vector2 tar = Target.position;
 
-        if ((cur - tar).magnitude <= DeadZone) return; // do nothing if target is within dead zone
+            if ((cur - tar).magnitude <= DeadZone) return; // do nothing if target is within dead zone
 
-        var targetPos = tar - (tar - cur).normalized * DeadZone; // target position is the edge of the dead zone
+            var targetPos = tar - (tar - cur).normalized * DeadZone; // target position is the edge of the dead zone
 
-        transform.position = Vector2.SmoothDamp(transform.position, targetPos, ref _velocity, MovementSmoothTime,
-            Mathf.Infinity, Time.deltaTime); // gradually move towards target
+            transform.position = Vector2.SmoothDamp(transform.position, targetPos, ref _velocity, MovementSmoothTime, Mathf.Infinity, Time.deltaTime); // gradually move towards target
 
-        transform.position = new Vector3(transform.position.x, transform.position.y, -10); // set z coordinate
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10); // set z coordinate
+        }
     }
 
     private void Zoom()
     {
-        var cam = GetComponent<Camera>();
-        var vel = Target.GetComponent<Rigidbody2D>().velocity.magnitude;
+        if (Target != null) {
+            var cam = GetComponent<Camera>();
+            var vel = Target.GetComponent<Rigidbody2D>().velocity.magnitude;
 
-        var targetSize = Mathf.Clamp(vel, MinSize, MaxSize); // use velocity as size, limit it
-        // to be between minSize and maxSize
+            var targetSize = Mathf.Clamp(vel, MinSize, MaxSize); // use velocity as size, limit it
+            // to be between minSize and maxSize
 
-        var zoomSmoothTime =
-            cam.orthographicSize < targetSize ? ZoomOutSmoothTime : ZoomInSmoothTime; // zoom at the appropriate rate
+            var zoomSmoothTime = cam.orthographicSize < targetSize ? ZoomOutSmoothTime : ZoomInSmoothTime; // zoom at the appropriate rate
 
-        cam.orthographicSize =
-            Mathf.SmoothDamp(cam.orthographicSize, targetSize, ref _zoomSpeed,
-                zoomSmoothTime); // gradually move towards target size
+            cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, targetSize, ref _zoomSpeed, zoomSmoothTime); // gradually move towards target size
+        }
     }
 
     private void Validate()
