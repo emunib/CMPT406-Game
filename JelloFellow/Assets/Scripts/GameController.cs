06c7f0420df4cdb58edd36c44546dba0a57e3eff
Acpeters <austin.clarke.peters@gmnail.com>
2018-03-01 19:01:07 -0600

    Added it so it remembers your high scores
    Changed SceneController to GameController

---
+++ JelloFellow/Assets/Scripts/GameController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public string previousSceneName = "";
+	public string currSceneName = "";
+	public static GameController control;
+	public HighScores highScores = new HighScores();
+	
+	// Use this for initialization
+	void Awake () {
+		if (control == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			control = this;
+		}
+		else if(control != this)
+		{
+			Destroy(gameObject);
+		}
+
+		currSceneName = SceneManager.GetActiveScene().name;
+		Load();
+	}
+	
+	public void Save()
+	{
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/highScores.dat");
+        
+		
+		if(!control.highScores.highScoreDictionary.ContainsKey(control.currSceneName))
+		{
+			control.highScores.highScoreDictionary.Add(control.currSceneName, "");
+		}
+		control.highScores.highScoreDictionary[control.currSceneName] = "" + Timer.timeToDisplay;
+		
+		HighScores scores = new HighScores();
+		scores = control.highScores;
+		
+		bf.Serialize(file,scores);
+		file.Close();
+	}
+
+	public void Load()
+	{
+		if (File.Exists(Application.persistentDataPath + "/highScores.dat"))
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/highScores.dat",FileMode.Open);
+			HighScores scores = (HighScores)bf.Deserialize(file);
+
+			control.highScores = scores;
+		}
+	}
+	
+	[Serializable]
+	public class HighScores
+	{
+		public Dictionary<String, String> highScoreDictionary = new Dictionary<string, string>();
+	}
+}
