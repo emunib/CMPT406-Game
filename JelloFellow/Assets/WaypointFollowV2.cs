503b816490e0f1ca70d5d4a35ec038a90fd2f989
Nicholas Brunoro <nicholasb373@gmail.com>
2018-03-09 20:25:39 -0600

    improved tracksaw. Is able to cmoplete waypoint array in circular fashion now

--- JelloFellow/Assets/WaypointFollowV2.cs
+++ JelloFellow/Assets/WaypointFollowV2.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Threading;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,39 +10,119 @@ public class WaypointFollowV2 : MonoBehaviour {
 
 	public Vector3[] LocalWayPoints;
 
+
+	public float SpinRatee;
+	public float Speed;
+
+	[Tooltip("If true, waypoints act in circular fashion. ie 0,1,2,3,0,1,2,3,0....")]
+	public bool CircularWaypoints;
 	
-	public float speed; 
+	
 	private Transform Saw;
+	
 	private int fromWaypointIndex = 0;
 
 	private float percentBetweenWaypoints = 0;
 
-	private Vector3[] GlobalWaypoints;
+	//private Vector3[] GlobalWaypoints;
+
+	private Vector2 _nextPos,_myPos;
+	
+	
+	
+	private bool _goingUp = true;
+	private int posCounter;
+
 	// Use this for initialization
 	void Start () {
 		Saw = transform.Find("Saw");
 
+		posCounter = 0;
+		
+		/*
 		GlobalWaypoints = new Vector3[LocalWayPoints.Length];
 		for (int i = 0; i < LocalWayPoints.Length; i++) {
 			GlobalWaypoints[i] = LocalWayPoints[i] + transform.position;
-		}
+		}*/
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 velocity = CalculateSawMovement();
-
+		
+		MoveSaw();
+		Spin();
+		
+		
+		/*Vector3 velocity = CalculateSawMoveSawment();
 		Saw.Translate(velocity);
-		
-		
+		Vector3 rotation = Saw.transform.localEulerAngles;
+		Saw.transform.localRotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z + 10);
+		*/
 
+		
+	}
+	
+	/// <summary>
+	/// MoveSaws the saw to the next position
+	/// </summary>
+	private void MoveSaw() {
+		Saw.localPosition= Vector3.MoveTowards(Saw.localPosition, _nextPos,Speed*Time.deltaTime);
+		
+		ChangeWaypoint();
 	}
 
-	private Vector3 CalculateSawMovement() {
+	private void Spin() {
+		Saw.transform.Rotate (0,0,-SpinRatee);
+
+	}
+	
+	
+	/// <summary>
+	/// Changes the waypointn to the next waypoint. If curcularwaypoints set to true go through waypoints array in
+	/// circular fashion
+	/// </summary>
+	private void ChangeWaypoint() {
+		if (Vector2.Distance(_nextPos, Saw.localPosition) <= 0.1) {
+
+			if (_goingUp) {
+				if (posCounter < LocalWayPoints.Length - 1) {
+					posCounter++;
+					
+					
+				}
+				else {
+
+					if (CircularWaypoints) {
+						posCounter = 0;
+					}
+					else {
+						_goingUp = false;
+
+					}
+
+				}
+			}
+			else {
+				if (posCounter > 0) {
+					posCounter--;
+
+				}
+				else {
+
+					_goingUp = true;
+				}
+			}
+
+			_nextPos = LocalWayPoints[posCounter];
+
+		}
+	}
+	/*
+	private Vector3 CalculateSawMoveSawment() {
 		int nextWaypointIndex = fromWaypointIndex + 1;
 		float distance = Vector3.Distance(GlobalWaypoints[fromWaypointIndex], GlobalWaypoints[nextWaypointIndex]);
-		percentBetweenWaypoints += Time.deltaTime * speed/distance;
+		percentBetweenWaypoints += Time.deltaTime * Speed/distance;
 
 		Vector3 newPos = Vector3.Lerp(GlobalWaypoints[fromWaypointIndex], GlobalWaypoints[nextWaypointIndex],
 			percentBetweenWaypoints);
@@ -49,7 +130,7 @@ public class WaypointFollowV2 : MonoBehaviour {
 		if (percentBetweenWaypoints >= 1) {
 			percentBetweenWaypoints = 0;
 			fromWaypointIndex++;
-			if (fromWaypointIndex >=GlobalWaypoints.Length) {
+			if (fromWaypointIndex >=GlobalWaypoints.Length-1) {
 				fromWaypointIndex = 0;
 				System.Array.Reverse(GlobalWaypoints);
 			}
@@ -58,7 +139,7 @@ public class WaypointFollowV2 : MonoBehaviour {
 		return newPos - Saw.transform.position;
 
 
-	}
+	}*/
 
 	private void OnDrawGizmos() {
 		if (LocalWayPoints != null) {
