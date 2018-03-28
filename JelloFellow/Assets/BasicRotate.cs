916217c76b2b21cd198c41f1aa8a4f6dbbf06cd2
Ptoasty <parker.neufeld@gmail.com>
2018-03-28 09:41:44 -0600

    Made a level called level_4b  it's a gone mad version of one of cody's levels and it will be a very fresh experience to those looking for more secrets and action. Also made it so the better letter rotates on the x axis to pull the user to them.

---
+++ JelloFellow/Assets/BasicRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BasicRotate : MonoBehaviour
+{
+	public float rate = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate (0,rate*Time.deltaTime,0); //rotates 50 degrees per second around z axis
+	}
+}
