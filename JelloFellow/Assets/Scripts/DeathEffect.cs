79640e3dc2d264341026dea74425257a8d951bec
Ehtasham <ehm011@mail.usask.ca>
2018-03-30 01:14:39 -0600

    disable collisions when player/enemy is dying
    also fixed bug where scene would reload multiple times

--- JelloFellow/Assets/Scripts/DeathEffect.cs
+++ JelloFellow/Assets/Scripts/DeathEffect.cs
@@ -3,27 +3,28 @@ using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(JellySprite))]
 [DisallowMultipleComponent]
-public class DeathEffect : MonoBehaviour {
+public class DeathEffect : MonoBehaviour
+{
     private JellySprite _jelly;
     private Vector2 _vel;
-    [Range(0, 0.5f)]public float ShrinkTime = 0.2f;
+    [Range(0, 0.5f)] public float ShrinkTime = 0.2f;
 
     private void Start()
     {
         _jelly = GetComponent<JellySprite>();
+        _jelly.CentralPoint.transform.parent.gameObject.SetActive(false);
     }
 
-    private void LateUpdate () {
+    private void LateUpdate()
+    {
         var scale = transform.localScale;
-        if (scale.x > 0.02f && scale.y > 0.02f)
+        if (scale.x <= 0.02f || scale.y <= 0.02f)
         {
-            foreach (var point in _jelly.ReferencePoints)
-            {
-                point.Body2D.velocity = Vector2.zero;
-            }
+            if (CompareTag("Player")) SceneLoader.instance.LoadSceneWithName(SceneManager.GetActiveScene().name);
+            Destroy(this);
         }
-        else SceneLoader.instance.LoadSceneWithName(SceneManager.GetActiveScene().name);
 
-        transform.localScale = Vector2.SmoothDamp(transform.localScale, Vector2.zero, ref _vel, ShrinkTime, Mathf.Infinity, Time.deltaTime);
+        transform.localScale = Vector2.SmoothDamp(transform.localScale, Vector2.zero, ref _vel, ShrinkTime,
+            Mathf.Infinity, Time.deltaTime);
     }
 }
